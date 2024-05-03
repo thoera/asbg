ebb11eb25f191f5d0d37d53eaf260d4db61e408d
Thomas Erard <erard.thomas@gmail.com>
2024-05-03 16:24:01 +0200

    Add a directory for the config files

--- src/asbg/utils/logger.py
+++ src/asbg/utils/logger.py
@@ -1,3 +1,4 @@
+import importlib.resources as pkg_resources
 import logging
 import logging.config
 from os import PathLike
@@ -6,20 +7,27 @@ import yaml
 
 
 def get_logger(
-    path: PathLike | str = "logging.yaml",
+    path: PathLike | str | None = None,
     name: str = __name__,
 ) -> logging.Logger:
     """Gets (or creates) a logger with the correct configuration.
 
     Args:
-        path: The path to the file with the config to use. Defaults to "logging.yaml".
+        path: The path to the config file to use. Defaults to None.
+            If None, the default config file is used.
         name: The name of the logger. Defaults to __name__.
 
     Returns:
         The logger with the specified name and configuration.
     """
-    with open(path, "r") as f:
-        cfg = yaml.safe_load(f.read())
+    if path is None:
+        ressource = pkg_resources.files("asbg.config").joinpath("logging.yaml")
+        with pkg_resources.as_file(ressource) as filename:
+            with open(filename, "r") as f:
+                cfg = yaml.safe_load(f.read())
+    else:
+        with open(path, "r") as f:
+            cfg = yaml.safe_load(f.read())
 
     logging.config.dictConfig(cfg)
 
